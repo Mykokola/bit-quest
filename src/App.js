6d34eb7a5bdd3a3454cf7a66b8f65861dbfb2e24
Mykokola <119622278+Mykokola@users.noreply.github.com>
2024-05-04 14:14:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 <h1>Старий бля буду гаманець не найобка</h1>
-
       </header>
     </div>
   );
